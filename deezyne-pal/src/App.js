c9b747f67dee0459b1eeb707700ae089e1849968
Vincent Snagg <vincent.snagg@gmail.com>
2019-02-07 20:21:23 -0500

    unignore Sketch files psh

---
+++ deezyne-pal/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="welcome">Welcome to the Deezyne PAL!</div>
+      </div>
+    );
+  }
+}
+
+export default App;
